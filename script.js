e6186e185dd71c6650f0cd9abf553c5fd2330c58
ScottGrieve <pokecarft@gmail.com>
2021-11-28 19:51:53 +0000

    Added expanding cards

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ const container = document.querySelector('.container');
 open.addEventListener('click', () => container.classList.add('show-nav'));
 
 close.addEventListener('click', () => container.classList.remove('show-nav'));
+
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
